86d2f1ea4d7b144f5332380c76609e5eaf3a3488
tcross7 <44528456+tcross7@users.noreply.github.com>
2022-12-18 17:50:13 -0500

    Updated 2022 with new tree and fire

--- christmas_lights.ino
+++ christmas_lights.ino
@@ -34,6 +34,19 @@ int fire_shifter = 0;
 bool scrolling = false;
 bool cooldown = false;
 
+// colors:
+
+// for the fire:
+int woodcolor = matrix.Color333(2,1,0);
+int fire1 = matrix.Color888(196,10,0); // R
+int fire2 = matrix.Color888(204,10,0); // R
+int fire3 = matrix.Color888(255,51,51); // R
+int fire4 = matrix.Color888(255,69,0); // O
+int fire5 = matrix.Color888(255,140,0); // O
+int fire6 = matrix.Color888(255,192,0); // Y
+int fire7 = matrix.Color888(244,196,48); // Y
+int fires[7] = {fire1,fire2,fire3,fire4,fire5,fire6,fire7};
+
 const char str[] PROGMEM = "Merry";
 const char str2[] PROGMEM = "      Christmas";
 int16_t    textX         = matrix.width(), // adjust for tree 
@@ -91,6 +104,8 @@ void loop() {
         fire_shifter = random(2);
     }
     draw_fire(fire_shifter);
+    
+    draw_lights();
        
     draw_dot_snowflake(flake1);
     draw_dot_snowflake(flake2);
@@ -106,15 +121,15 @@ void loop() {
     draw_dot_snowflake(flake12);
     
 
-//    scroll_christmas();
-//    if (millis() % 1000 == 0){
-//        cooldown = false;
-//    }
-//    
-//    if ((random(10) == 0) && (cooldown == false)){ // 10% chance - play with this
-//        scrolling = true;
-//        cooldown = true;
-//    }
+    scroll_christmas();
+    if (millis() % 1000 == 0){
+        cooldown = false;
+    }
+    
+    if ((random(10) == 0) && (cooldown == false)){ // 10% chance - play with this
+        scrolling = true;
+        cooldown = true;
+    }
 
     // HOW TO PRINT CHARACTERS
     //    matrix.setCursor(1, 9);  // set the cursor
@@ -126,55 +141,75 @@ void loop() {
 
 
 // Active functions
-void draw_fire(int shifter)
+void draw_lights(void)
 {
-    int NUMBRN = 4;
-    int NUMRED = 4;
-    int NUMYEL = 1;
+    int blue_x[4] = {24, 22, 26, 19};
+    int blue_y[4] = {2, 5, 8, 11};
+    int blue_color = matrix.Color888(75,100,255); // 'light sky blue'
 
-    int woodcolor = matrix.Color333(2,1,0);
-    
-    int fire1 = matrix.Color888(196,10,0); // R
-    int fire2 = matrix.Color888(204,10,0); // R
-    int fire3 = matrix.Color888(255,51,51); // R
-    int fire4 = matrix.Color888(255,69,0); // O
-    int fire5 = matrix.Color888(255,140,0); // O
-    int fires[5] = {fire1,fire2,fire3,fire4,fire5};
-    
-    int white = matrix.Color888(255,255,255);
-    int blank = matrix.Color888(0,0,0);
-    
-    int yellowcolor1 = matrix.Color888(255,255,51);
-    int yellowcolor2 = matrix.Color888(255,255,0);
-    int yellows[2] = {yellowcolor1, yellowcolor2};
+    int yellow_light_x[3] = {24, 23, 27};
+    int yellow_light_y[3] = {5, 8, 10};
+    int yellow_light_color = matrix.Color888(255,150,0); // 'lightyellow2'
 
+    int red_light_x[3] = {26, 21, 29};
+    int red_light_y[3] = {5, 10, 11};
+    int red_light_color = matrix.Color888(139,0,0); //'darkred'
 
-    int xlist_wood[NUMBRN] = {12,13,14,15};
-    int ylist_wood[NUMBRN] = {14,14,14,14};
-
-    int xlist_red[NUMRED] = {13,14,
-                             13,14};
-    int ylist_red[NUMRED] = {12, 12,
-                             13, 13};
+    int purple_x[3] = {20, 25, 22};
+    int purple_y[3] = {7, 11, 12};
+    int purple_color = matrix.Color888(218,30,214); // 'orchid'
     
-    int xlist_yellow[NUMYEL+1] = {13, 14};
-    int ylist_yellow[NUMYEL] = {11};
 
     if (scrolling == false)
     {
-        for (int i = 0 ; i < NUMBRN ; i++ ){
-              matrix.drawPixel(xlist_wood[i], ylist_wood[i], woodcolor);
-              if (i < NUMRED)
-              {
-                  matrix.drawPixel(xlist_red[i], ylist_red[i], fires[random(5)]);
-              }
-              if (i < NUMYEL)
-              {
-                  matrix.drawPixel(xlist_yellow[i+shifter], ylist_yellow[i], fires[random(5)]);
-              }
-          }
+        drawArray(blue_x, sizeof(blue_x), blue_y, sizeof(blue_y), blue_color, 0);
+        drawArray(yellow_light_x, sizeof(yellow_light_x), yellow_light_y, sizeof(yellow_light_y), yellow_light_color, 0);
+        drawArray(red_light_x, sizeof(red_light_x), red_light_y, sizeof(red_light_y), red_light_color, 0);
+        drawArray(purple_x, sizeof(purple_x), purple_y, sizeof(purple_y), purple_color, 0);
     }
+}
+void draw_fire(int shifter)
+{
+    int NUMBRN = 8;
+    int NUMRED = 2;
+    int NUMORG = 2;
+    int NUMYEL = 1;
     
+    // fire base
+    int xlist_wood[NUMBRN] = {11,12,13,14,
+                              11,12,13,14};
+    int ylist_wood[NUMBRN] = {13,13,13,13,
+                              14,14,14,14};
+
+    // red fire
+    int xlist_red[NUMRED] = {12,13};
+    int ylist_red[NUMRED] = {12,12};
+    
+    // orange fire
+    int xlist_orange[NUMORG] = {12,13};
+    int ylist_orange[NUMORG] = {11,11};
+
+    // yellow fire
+    int xlist_yellow[NUMYEL+1] = {12, 13};
+    int ylist_yellow[NUMYEL] = {10};
+
+    if (scrolling == false){
+        for (int i = 0 ; i < NUMBRN ; i++ ){
+            matrix.drawPixel(xlist_wood[i], ylist_wood[i], woodcolor);
+            if (i < NUMRED)
+            {
+                matrix.drawPixel(xlist_red[i], ylist_red[i], fires[random(7)]);
+            }
+            if (i < NUMORG)
+            {
+                matrix.drawPixel(xlist_orange[i], ylist_orange[i], fires[random(7)]);
+            }
+            if (i < NUMYEL)
+            {
+                matrix.drawPixel(xlist_yellow[i+shifter], ylist_yellow[i], fires[random(7)]);
+            }
+        }
+    }
 }
 
 void draw_reindeer()
@@ -282,7 +317,8 @@ void draw_reindeer_down()
     }
 }
 
-void draw_dot_snowflake(Snowflake input_flake) // draw a dot snowflake, under any circumstance
+// draw a dot snowflake, under any circumstance
+void draw_dot_snowflake(Snowflake input_flake) 
 {
     int x = input_flake.x;
     int y = input_flake.y;
@@ -291,52 +327,59 @@ void draw_dot_snowflake(Snowflake input_flake) // draw a dot snowflake, under an
     matrix.drawPixel(x   , y   , color); 
 }
 
-  // draw the tree, only under steady state
-  void draw_tree()
-  {
-      int NUMGRN = 50;
-      int NUMBRN = 4;
-      
-      int tree_brown = matrix.Color333(2,1,0);
-      int tree_green = matrix.Color333(0,3,1);
-      int x_list_green[NUMGRN] =         {27,
-                                       26,27,28,
-                                    25,26,27,28,29,
-                                 24,25,26,27,28,29,30,
-                              23,24,25,26,27,28,29,30,31,
-                                 24,25,26,27,28,29,30,
-                              23,24,25,26,   28,29,30,31,
-                           22,23,24,25,26,   28,29,30,31,32}; 
-                              
-      int y_list_green[NUMGRN] = {       7,
-                                       8,8,8,
-                                     9,9,9,9,9,
-                                10,10,10,10,10,10,10,
-                              11,11,11,11,11,11,11,11,11,
-                                 12,12,12,12,12,12,12,
-                              13,13,13,13,   13,13,13,13,
-                           14,14,14,14,14,   14,14,14,14,14};
-      
-      int x_list_brown[NUMBRN] = {27,27,27,27};
-      int y_list_brown[NUMBRN] = {13,14,15,16};
-  
-  
-      if (scrolling == false) {
-          for (int i = 0 ; i < NUMGRN ; i++ ){
-              matrix.drawPixel(x_list_green[i]-2, y_list_green[i]-2, tree_green);
-              if (i < NUMBRN){
-                  matrix.drawPixel(x_list_brown[i]-2, y_list_brown[i]-2, tree_brown);
-              }
-          }
-      }
-  }
+// draw the tree, only under steady state
+void draw_tree()
+{
+    int NUMGRN = 86;
+    int NUMBRN = 4;
+    
+    int tree_brown = matrix.Color333(2,1,0);
+    int tree_green = matrix.Color888(34, 139, 34); // 'forest green'
+
+    int x_list_green[NUMGRN] =         {23,24,
+                                    22,23,24,25,
+                                21,22,23,24,25,26,
+                                20,21,22,23,24,25,26,27,
+                                21,22,23,24,25,26,
+                                20,21,22,23,24,25,26,27,
+                            19,20,21,22,23,24,25,26,27,28,
+                                20,21,22,23,24,25,26,27,
+                            19,20,21,22,23,24,25,26,27,28,
+                        18,19,20,21,22,23,24,25,26,27,28,29,
+                        18,19,20,21,22,23,24,25,26,27,28,29}; 
+                            
+    int y_list_green[NUMGRN] =         {2,2,
+                                    3,3,3,3,
+                                4,4,4,4,4,4,
+                                5,5,5,5,5,5,5,5,
+                                6,6,6,6,6,6,
+                                7,7,7,7,7,7,7,7,
+                            8,8,8,8,8,8,8,8,8,8,
+                                9,9,9,9,9,9,9,9,
+                            10,10,10,10,10,10,10,10,10,10,
+                        11,11,11,11,11,11,11,11,11,11,11,11,
+                        12,12,12,12,12,12,12,12,12,12,12,12};
+    
+    int x_list_brown[NUMBRN] = {23,24,23,24};
+    int y_list_brown[NUMBRN] = {13,13,14,14};
+
+
+    if (scrolling == false) {
+        for (int i = 0 ; i < NUMGRN ; i++ ){
+            matrix.drawPixel(x_list_green[i], y_list_green[i], tree_green);
+            if (i < NUMBRN){
+                matrix.drawPixel(x_list_brown[i], y_list_brown[i], tree_brown);
+            }
+        }
+    }
+}
 
 // draw the ground, only under steady state
 void draw_ground()
   { 
       if (scrolling == false){
           for (int i = 0 ; i < 32 ; i++){
-              matrix.drawPixel(i,15,matrix.Color333(1,1,1));
+              matrix.drawPixel(i,15,matrix.Color333(3,3,3));
           }
       }
   }
@@ -361,7 +404,83 @@ void scroll_christmas()
       }
   }
 
+// draw a single point with a given color
+void drawPoint(int x, int y, int color)
+{
+    matrix.drawPixel(x, y, color);
+}
+
+// draw a series of points with a given color
+int drawArray(int xarray[], int size_x, int yarray[], int size_y, int color, int pause)
+{
+    int x_length = size_x/sizeof(xarray[0]);
+    int y_length = size_y/sizeof(yarray[0]);
+    
+    if (x_length != y_length)
+    {
+        return 0;
+    }
+
+    for (int i = 0 ; i < x_length ; i++)
+    {
+        matrix.drawPixel(xarray[i], yarray[i], color);
+        delay(pause);
+    }
+    return 1;
+}
+
 // Inactive functions
+void draw_fire_old(int shifter)
+{
+    int NUMBRN = 4;
+    int NUMRED = 4;
+    int NUMYEL = 1;
+
+    int woodcolor = matrix.Color333(2,1,0);
+    
+    int fire1 = matrix.Color888(196,10,0); // R
+    int fire2 = matrix.Color888(204,10,0); // R
+    int fire3 = matrix.Color888(255,51,51); // R
+    int fire4 = matrix.Color888(255,69,0); // O
+    int fire5 = matrix.Color888(255,140,0); // O
+    int fires[5] = {fire1,fire2,fire3,fire4,fire5};
+    
+    int white = matrix.Color888(255,255,255);
+    int blank = matrix.Color888(0,0,0);
+    
+    int yellowcolor1 = matrix.Color888(255,255,51);
+    int yellowcolor2 = matrix.Color888(255,255,0);
+    int yellows[2] = {yellowcolor1, yellowcolor2};
+
+
+    int xlist_wood[NUMBRN] = {12,13,14,15};
+    int ylist_wood[NUMBRN] = {14,14,14,14};
+
+    int xlist_red[NUMRED] = {13,14,
+                             13,14};
+    int ylist_red[NUMRED] = {12, 12,
+                             13, 13};
+    
+    int xlist_yellow[NUMYEL+1] = {13, 14};
+    int ylist_yellow[NUMYEL] = {11};
+
+    if (scrolling == false)
+    {
+        for (int i = 0 ; i < NUMBRN ; i++ ){
+              matrix.drawPixel(xlist_wood[i], ylist_wood[i], woodcolor);
+              if (i < NUMRED)
+              {
+                  matrix.drawPixel(xlist_red[i], ylist_red[i], fires[random(5)]);
+              }
+              if (i < NUMYEL)
+              {
+                  matrix.drawPixel(xlist_yellow[i+shifter], ylist_yellow[i], fires[random(5)]);
+              }
+          }
+    }
+    
+}
+
 void draw_star()
 {
     int x = 6;
